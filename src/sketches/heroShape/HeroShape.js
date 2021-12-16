afd0de9c5609f84fe70eb8030ba8c5e5c8f8d9fa
Gui Silva <guijsilva4@gmail.com>
2021-12-15 19:06:29 -0800

    Added slider to Hero sketch.

--- src/sketches/heroShape/HeroShape.js
+++ src/sketches/heroShape/HeroShape.js
@@ -1,43 +1,45 @@
 import Sketch from 'react-p5';
 import Dasher from './dasher';
+import Slider from './slider';
 
 let d;
-let col;
 let parentRef;
-const HeroShape = () => {
+let slider;
+const HeroShape = (props) => {
   const setup = (p5, canvasParentRef) => {
     parentRef = canvasParentRef;
     const w = canvasParentRef.clientWidth;
     const h = canvasParentRef.clientHeight;
-    const s = p5.floor(p5.min(w, h));
-    p5.createCanvas(s, s).parent(canvasParentRef);
-    col = p5.color(22, 22, 27);
+    p5.createCanvas(w, h).parent(canvasParentRef);
     p5.angleMode(p5.DEGREES);
-    d = new Dasher(p5, s / 2.6, p5.width / 2, p5.height / 2);
+    p5.frameRate(30);
+    d = new Dasher(p5, parentRef, h / 5, p5.width / 2, p5.height / 2);
+    slider = new Slider(
+      p5,
+      p5.width - 25,
+      (props.sliderPos * p5.height) / 6,
+      15,
+      30,
+      4,
+      props.sliderPos,
+      props.setSliderPos
+    );
   };
 
   const draw = (p5) => {
-    p5.background(col);
+    p5.background(22, 22, 27);
     d.update();
-    d.render();
-  };
-
-  const windowResized = (p5) => {
-    if (parentRef === undefined) {
-      return;
-    }
-    const h = p5.max(parentRef.clientHeight, 100);
-    const w = p5.max(parentRef.clientWidth, 100);
-    const s = p5.min(w, h);
-    d = new Dasher(p5, s / 2.6, p5.width / 2, p5.height / 2);
-    p5.resizeCanvas(s, s);
+    d.render(props.sliderPos);
+    const alpha = d.showSlider ? 255 : 0;
+    slider.handleClick();
+    slider.update();
+    slider.render(alpha);
   };
 
   return (
     <Sketch
       setup={setup}
       draw={draw}
-      windowResized={windowResized}
       style={{
         position: 'relative',
         width: '100%',
--- src/sketches/heroShape/dasher.js
+++ src/sketches/heroShape/dasher.js
@@ -1,53 +1,73 @@
 export default class Dasher {
-  constructor(p5, radius, x, y) {
+  constructor(p5, parent, radius, x, y) {
     this.p5 = p5;
+    this.parent = parent;
     this.r = radius;
     this.pos = p5.createVector(x, y);
+    this.showSlider = false;
     this.segments = [];
-    for (let i = 0; i >= -360; i--) {
+    const r1 = this.r;
+    const r2 = (this.r * 5) / 8;
+    for (let i = 0; i >= -1800; i -= 5) {
       this.segments.push(
-        p5.createVector(radius * p5.cos(5 * i), radius * p5.sin(7 * i), i)
+        this.p5.createVector(
+          (r1 + r2) * this.p5.cos(i) - r2 * this.p5.cos((r1 / r2 + 1) * i),
+          (r1 + r2) * this.p5.sin(i) - r2 * this.p5.sin((r1 / r2 + 1) * i),
+          i
+        )
       );
     }
   }
 
   update() {
-    const a = this.segments[0].z + 1;
+    const a = this.segments[0].z + 5;
+    const r1 = this.r;
+    const r2 = (this.r * 5) / 8;
     this.segments.unshift(
       this.p5.createVector(
-        this.r * this.p5.cos(5 * a),
-        this.r * this.p5.sin(7 * a),
+        (r1 + r2) * this.p5.cos(a) - r2 * this.p5.cos((r1 / r2 + 1) * a),
+        (r1 + r2) * this.p5.sin(a) - r2 * this.p5.sin((r1 / r2 + 1) * a),
         a
       )
     );
     this.segments.pop();
   }
 
-  render() {
+  render(step) {
+    const rect = this.parent.getBoundingClientRect();
+    let d = this.p5.map(
+      this.p5.dist(
+        this.p5.winMouseX,
+        this.p5.winMouseY,
+        rect.left + this.p5.width / 2,
+        rect.top + this.p5.height / 2
+      ),
+      0,
+      this.p5.width,
+      0,
+      100
+    );
+    if (d > 100) {
+      d = 100;
+    } else if (
+      d < 0 ||
+      this.p5.winMouseX - (rect.left + this.p5.width / 2) > 0
+    ) {
+      this.showSlider = true;
+      d = 0;
+    } else {
+      this.showSlider = false;
+    }
+    let alpha = this.p5.map(d, 0, 100, 0.2, 0.01);
+    if (alpha > 0.2) {
+      alpha = 0.2;
+    } else if (alpha < 0.01) {
+      alpha = 0.01;
+    }
     this.p5.push();
-    this.p5.colorMode(this.p5.HSB);
     for (let i = 0; i < this.segments.length - 1; i++) {
-      const d = this.p5.map(
-        this.p5.dist(
-          this.p5.mouseX,
-          this.p5.mouseY,
-          this.p5.width / 2,
-          this.p5.height / 2
-        ),
-        0,
-        this.p5.width,
-        100,
-        0
-      );
-      let alpha = this.p5.map(d, 100, 0, 0.3, 0.01);
-      if (alpha > 0.3) {
-        alpha = 0.3;
-      } else if (alpha < 0.01) {
-        alpha = 0.01;
-      }
-
-      this.p5.stroke(i, d, 100, alpha);
-
+      this.p5.colorMode(this.p5.HSB);
+      this.p5.stroke((i * step) % 360, 100 - d, 100, alpha);
       this.p5.line(
         this.pos.x + this.segments[i].x,
         this.pos.y + this.segments[i].y,
@@ -56,14 +76,5 @@ export default class Dasher {
       );
     }
     this.p5.pop();
-  }
-
-  setPos(x, y) {
-    this.pos.x = x;
-    this.pos.y = y;
-  }
-
-  setRadius(r) {
-    this.r = r;
   }
 }
